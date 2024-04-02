5c7f5337e7b0043226658c5a9d4269a7b8e07cb3
Derek Guenther <derek@iflabs.network>
2024-04-02 10:23:21 -0400

    Remove unused code from the native module (#9)

--- packages/ironfish-native-module/android/src/main/java/expo/modules/ironfishnativemodule/IronfishNativeModuleView.kt
+++
@@ -1,7 +0,0 @@
-package expo.modules.ironfishnativemodule
-
-import android.content.Context
-import expo.modules.kotlin.AppContext
-import expo.modules.kotlin.views.ExpoView
-
-class IronfishNativeModuleView(context: Context, appContext: AppContext) : ExpoView(context, appContext)
