0716d493c0bceb5e385ad97df31c8606bf5a6905
Daniel Cortes <danielgcortes@gmail.com>
2024-04-04 16:50:09 -0400

    Implement query functionality

---
+++ packages/data-facade/src/index.ts
@@ -0,0 +1,71 @@
+import { ZodTypeAny, z } from "zod";
+import {
+  UndefinedInitialDataOptions,
+  UseQueryResult,
+  useQuery,
+} from "@tanstack/react-query";
+
+type UseQueryOptions = UndefinedInitialDataOptions<any, Error, any, unknown[]>;
+type ResolverFunc<T = any> = (opts: T) => any;
+type UseQueryType<TResolver extends ResolverFunc> =
+  Parameters<TResolver>["length"] extends 0
+    ? (
+        args?: null,
+        opts?: UseQueryOptions,
+      ) => UseQueryResult<ReturnType<TResolver>>
+    : (
+        args: Parameters<TResolver>[0],
+        opts?: UseQueryOptions,
+      ) => UseQueryResult<ReturnType<TResolver>>;
+
+function buildUseQuery(baseQueryKey: string) {
+  return <TResolver extends ResolverFunc>(resolver: TResolver) => ({
+    useQuery: (args?: unknown) => {
+      return useQuery({
+        queryKey: [baseQueryKey],
+        queryFn: () => resolver(args),
+      });
+    },
+  });
+}
+
+function handlerQuery<TResolver extends ResolverFunc>(
+  func: TResolver,
+): (baseQueryKey: string) => {
+  useQuery: UseQueryType<TResolver>;
+} {
+  return (baseQueryKey: string) => buildUseQuery(baseQueryKey)(func);
+}
+
+function facade<
+  THandlers extends Record<string, ReturnType<typeof handlerQuery>>,
+>(handlers: THandlers) {
+  const result: Record<string, any> = {};
+
+  for (const [key, handler] of Object.entries(handlers)) {
+    result[key] = handler(key);
+  }
+
+  return result as { [K in keyof THandlers]: ReturnType<THandlers[K]> };
+}
+
+function input<TSchema extends ZodTypeAny>(_schema: TSchema) {
+  return {
+    query: <TResolver extends ResolverFunc<z.infer<TSchema>>>(
+      resolver: TResolver,
+    ) => {
+      return handlerQuery(resolver);
+    },
+  };
+}
+
+const handler = {
+  input,
+  query: handlerQuery,
+  mutation: (options: any) => {},
+};
+
+export const f = {
+  facade,
+  handler,
+};
--- packages/mobile-app/app/(tabs)/transact.tsx
+++ packages/mobile-app/app/(tabs)/transact.tsx
@@ -1,6 +1,46 @@
 import { View, Text } from "react-native";
+import { z } from "zod";
+import { f } from "data-facade";
+
+const accounts = ["alice", "bob", "carol"];
+
+async function getAccounts(limit: number) {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  return accounts.slice(0, limit);
+}
+
+const accountsFacade = f.facade({
+  getAccounts: f.handler.query(async (count: number) => {
+    const accounts = await getAccounts(count ?? 1);
+    console.log("getAccounts", accounts);
+    return accounts;
+  }),
+  getAllAccounts: f.handler.query(async () => {
+    const accounts = await getAccounts(1);
+    console.log("getAllAccounts", accounts);
+    return accounts;
+  }),
+  getAccountsWithZod: f.handler
+    .input(
+      z.object({
+        limit: z.number(),
+      }),
+    )
+    .query(async ({ limit }) => {
+      const accounts = await getAccounts(limit);
+      console.log("getAccountsWithZod", accounts);
+      return accounts;
+    }),
+});
 
 export default function Transact() {
+  // @ts-expect-error
+  const getAccountsResult = accountsFacade.getAccounts.useQuery();
+  const getAccountsWithZodResult = accountsFacade.getAccountsWithZod.useQuery({
+    limit: 2,
+  });
+  const getAllAccountsResult = accountsFacade.getAllAccounts.useQuery();
+
   return (
     <View>
       <Text>Balances</Text>
