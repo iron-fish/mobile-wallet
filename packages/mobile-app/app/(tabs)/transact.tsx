95fd47b517c9baa722aabaffbe747b9dfb663deb
Daniel Cortes <danielgcortes@gmail.com>
2024-04-10 15:58:22 -0600

    Tweak example

--- packages/mobile-app/app/(tabs)/transact.tsx
+++ packages/mobile-app/app/(tabs)/transact.tsx
@@ -1,11 +1,9 @@
 import { View, Text } from "react-native";
 import { useFacade } from "../../data";
-import { useMutation } from "@tanstack/react-query";
 import { Button } from "@ironfish/ui";
 import { useState } from "react";
 
 export default function Transact() {
-  const [vanillaResult, setVanillaResult] = useState(0);
   const [facadeResult, setFacadeResult] = useState([""]);
 
   const facade = useFacade();
@@ -17,24 +15,15 @@ export default function Transact() {
 
   const createAccount = facade.createAccount.useMutation();
 
-  const vanillaMutation = useMutation({
-    mutationFn: async () => {
-      return 777;
-    },
-  });
-
   return (
     <View>
       <Text>Accounts</Text>
       <Text>{JSON.stringify(getAccountsResult.data)}</Text>
       <Text>{JSON.stringify(getAccountsWithZodResult.data)}</Text>
       <Text>{JSON.stringify(getAllAccountsResult.data)}</Text>
-      <Text>{vanillaResult}</Text>
-      <Text>{facadeResult}</Text>
+      <Text>Mutation: {facadeResult}</Text>
       <Button
         onClick={async () => {
-          const vanillaResult = await vanillaMutation.mutateAsync();
-          setVanillaResult(vanillaResult);
           const otherResult = await createAccount.mutateAsync("dave");
           setFacadeResult(otherResult);
         }}
