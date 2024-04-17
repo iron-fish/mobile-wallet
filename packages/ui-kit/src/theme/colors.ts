24d20873ccab9cc295a749eb963dd7737f2640ea
Daniel Cortes <danielgcortes@gmail.com>
2024-04-17 13:40:55 -0600

    About to install fonts

---
+++ packages/ui-kit/src/theme/colors.ts
@@ -0,0 +1,16 @@
+const COLORS = {
+  light: {
+    foreground: "#000000",
+    background: "#FFFFFF",
+    gray1: "#7F7F7F",
+    gray2: "#DEDFE2",
+    gray3: "#F3F3F4",
+  },
+  dark: {
+    foreground: "#FFFFFF",
+    background: "#101010",
+    gray1: "#ADAEB4",
+    gray2: "#3B3B3B",
+    gray3: "#252525",
+  },
+};
---
+++ packages/ui-kit/src/theme/typography.ts
@@ -0,0 +1,42 @@
+export const FONT_VARIANTS = {
+  xs: {
+    fontFamily: "Favorit",
+    fontSize: 12,
+    lineHeight: 19,
+  },
+  sm: {
+    fontFamily: "Favorit",
+    fontSize: 16,
+    lineHeight: 21,
+  },
+  md: {
+    fontFamily: "Favorit",
+    fontSize: 20,
+    lineHeight: 27,
+  },
+  lg: {
+    fontFamily: "Favorit",
+    fontSize: 24,
+    lineHeight: 26,
+  },
+  xl: {
+    fontFamily: "Favorit",
+    fontSize: 32,
+    lineHeight: 36,
+  },
+  "2xl": {
+    fontFamily: "Favorit",
+    fontSize: 40,
+    lineHeight: 54,
+  },
+  "3xl": {
+    fontFamily: "FavoritExtended",
+    fontSize: 44,
+    lineHeight: 50,
+  },
+  "4xl": {
+    fontFamily: "FavoritExtended",
+    fontSize: 70,
+    lineHeight: 62,
+  },
+};
