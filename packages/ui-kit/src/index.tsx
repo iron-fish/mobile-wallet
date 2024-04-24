c526ffa0ac034702d9793d5dacf7d651118c3093
Daniel Cortes <danielgcortes@gmail.com>
2024-04-24 16:54:22 -0600

    Clean up ui-kit index file

--- packages/ui-kit/src/index.tsx
+++ packages/ui-kit/src/index.tsx
@@ -1,8 +1,5 @@
-// export * from "tamagui";
 export * from "@tamagui/toast";
-// export * from './MyComponent'
-// export * from './CustomToast'
-export { config } from "./theme/config";
 
+export { config } from "./theme/config";
 export * from "./UIKitProvider/UIKitProvider";
 export * from "./Button/Button";
