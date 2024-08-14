9ec7dd658452eaef1818856faf7283ff15bf447f
Derek Guenther <dguenther9@gmail.com>
2024-08-14 13:04:14 -0400

    Add ignore to lint rule

--- packages/mobile-app/.eslintrc.js
+++ packages/mobile-app/.eslintrc.js
@@ -3,6 +3,9 @@ module.exports = {
   extends: ["expo", "prettier"],
   plugins: ["prettier"],
   rules: {
+    // This rule fails if @ironfish/ui hasn't been built yet, so we can disable it
+    // specifically for that package.
+    "import/no-unresolved": ["error", { ignore: ["^@ironfish/ui"] }],
     "prettier/prettier": "error",
   },
 };
