e6f85cfe672ca424b7862e0ec92de394a58e1164
Derek Guenther <dguenther9@gmail.com>
2024-08-14 15:55:07 -0400

    Use Nx for linting and typechecking

--- packages/mobile-app/.eslintrc.js
+++ packages/mobile-app/.eslintrc.js
@@ -3,9 +3,6 @@ module.exports = {
   extends: ["expo", "prettier"],
   plugins: ["prettier"],
   rules: {
-    // This rule fails if @ironfish/ui hasn't been built yet, so we can disable it
-    // specifically for that package.
-    "import/no-unresolved": ["error", { ignore: ["^@ironfish/ui"] }],
     "prettier/prettier": "error",
   },
 };
