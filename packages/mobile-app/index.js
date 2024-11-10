3a29a9a8dedbbe9c075b2bbea630b2b323f6a57e
Daniel Cortes <danielgcortes@gmail.com>
2024-11-09 17:51:49 -0700

    Save

--- packages/mobile-app/index.js
+++ packages/mobile-app/index.js
@@ -3,10 +3,8 @@ global.process = require("process");
 
 require("expo-router/entry");
 
-const ReactNativeFeatureFlags = require("react-native/Libraries/ReactNative/ReactNativeFeatureFlags");
+// const ReactNativeFeatureFlags = require("react-native/Libraries/ReactNative/ReactNativeFeatureFlags");
 
-console.log("SETTING THE THINGS");
-
-ReactNativeFeatureFlags.shouldEmitW3CPointerEvents = () => true;
-ReactNativeFeatureFlags.shouldPressibilityUseW3CPointerEventsForHover = () =>
-  true;
+// ReactNativeFeatureFlags.shouldEmitW3CPointerEvents = () => true;
+// ReactNativeFeatureFlags.shouldPressibilityUseW3CPointerEventsForHover = () =>
+//   true;
