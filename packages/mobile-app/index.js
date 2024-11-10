44079c1df6503d6e8c1e74d796ffcf0ec08509d5
Daniel Cortes <danielgcortes@gmail.com>
2024-11-09 23:56:00 -0700

    Enable new architecture

--- packages/mobile-app/index.js
+++ packages/mobile-app/index.js
@@ -2,3 +2,9 @@ global.Buffer = require("buffer").Buffer;
 global.process = require("process");
 
 require("expo-router/entry");
+
+const ReactNativeFeatureFlags = require("react-native/Libraries/ReactNative/ReactNativeFeatureFlags");
+
+ReactNativeFeatureFlags.shouldEmitW3CPointerEvents = () => true;
+ReactNativeFeatureFlags.shouldPressibilityUseW3CPointerEventsForHover = () =>
+  true;
