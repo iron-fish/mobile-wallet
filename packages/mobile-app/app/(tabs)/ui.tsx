8b1b8b3617487e74417cae5bf7b77ca78b646869
Daniel Cortes <danielgcortes@gmail.com>
2024-11-15 12:41:02 -0800

    Save

--- packages/mobile-app/app/(tabs)/ui.tsx
+++ packages/mobile-app/app/(tabs)/ui.tsx
@@ -1,10 +1,20 @@
-import { useColorScheme, View, Text } from "react-native";
+import * as Font from "expo-font";
+import { useColorScheme, View, Text, StyleSheet } from "react-native";
 import { Button, Box, HStack, VStack } from "@ironfish/tackle-box";
 import { useState } from "react";
+
+const styles = StyleSheet.create({
+  base: {
+    fontFamily: "Favorit",
+  },
+});
 
 export default function UiKit() {
   const scheme = useColorScheme();
   const [count, setCount] = useState(0);
+
+  const blah = Font.isLoaded("ABCFavorit-Regular");
+  console.log({ blah });
 
   return (
     <View
@@ -14,7 +24,7 @@ export default function UiKit() {
         backgroundColor: scheme === "dark" ? "#333" : "#fff",
       }}
     >
-      <Text>Count: {count}</Text>
+      <Text style={styles.base}>Count: {count}</Text>
       <Button title="Press me" onClick={() => setCount(count + 1)} />
       <Box height="auto" bg="pink" borderWidth={2} borderColor="gray">
         <Button title="Press me" onClick={() => setCount(count + 1)} />
