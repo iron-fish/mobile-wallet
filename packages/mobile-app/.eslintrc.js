83de12a50a88d5628447112ad31189f10f4f8cc6
Daniel Cortes <danielgcortes@gmail.com>
2024-12-02 16:32:01 -0700

    Turn off import eslint rule

--- packages/mobile-app/.eslintrc.js
+++ packages/mobile-app/.eslintrc.js
@@ -1,13 +1,9 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
   extends: ["expo", "prettier"],
-  plugins: ["prettier", "plugin:import/typescript"],
+  plugins: ["prettier"],
   rules: {
     "prettier/prettier": "error",
-  },
-  settings: {
-    "import/resolver": {
-      typescript: {},
-    },
+    "import/no-unresolved": "off",
   },
 };
