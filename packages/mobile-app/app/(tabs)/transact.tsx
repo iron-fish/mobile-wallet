49a6b0a14ccaa185c47d9c9e528981caf1d1b351
Daniel Cortes <danielgcortes@gmail.com>
2025-01-24 14:01:00 -0700

    Update project to Expo 52

--- packages/mobile-app/app/(tabs)/transact.tsx
+++ packages/mobile-app/app/(tabs)/transact.tsx
@@ -1,5 +1,4 @@
-import { Modal, SafeAreaView, Button } from "react-native";
-import { router } from "expo-router";
+import { SafeAreaView } from "react-native";
 import { Text } from "react-native";
 import { LinkButton } from "../../components/LinkButton";
 
@@ -7,18 +6,13 @@ import { LinkButton } from "../../components/LinkButton";
  * This is a placeholder page to show a modal with the Transact options.
  */
 export default function Transact() {
-  const isPresented = router.canGoBack();
-
   return (
-    <Modal animationType="slide">
-      <SafeAreaView style={{ flex: 1 }}>
-        <Text>Transact</Text>
-        <LinkButton title="Receive" href="/address/" />
-        <Text>Receive assets from another wallet</Text>
-        <LinkButton title="Send" href="/send/" />
-        <Text>Send assets to another wallet</Text>
-        {isPresented && <Button title="Close" onPress={() => router.back()} />}
-      </SafeAreaView>
-    </Modal>
+    <SafeAreaView style={{ flex: 1 }}>
+      <Text>Transact</Text>
+      <LinkButton title="Receive" href="/address/" />
+      <Text>Receive assets from another wallet</Text>
+      <LinkButton title="Send" href="/send/" />
+      <Text>Send assets to another wallet</Text>
+    </SafeAreaView>
   );
 }
