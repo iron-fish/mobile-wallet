939602f10cda58fbf199fcb03689190ea7ed2e0d
Derek Guenther <derek@iflabs.network>
2025-02-06 14:52:11 -0500

    Add Settings DB (#105)

--- packages/mobile-app/data/facades/app/oreowalletHandlers.ts
+++ packages/mobile-app/data/facades/app/oreowalletHandlers.ts
@@ -2,36 +2,34 @@ import { f } from "data-facade";
 import { AppHandlers, AppSettings } from "./types";
 
 import { oreoWallet } from "../../wallet/oreowalletWallet";
+import { SettingsKey } from "@/data/settings/db";
+import { SettingsManager } from "@/data/settings/manager";
+import { Network } from "@/data/constants";
 
 export const appHandlers = f.facade<AppHandlers>({
   loadDatabases: f.handler.mutation(async () => {
     if (oreoWallet.state.type !== "STARTED") {
       await oreoWallet.start();
     }
+    await SettingsManager.start();
     return "loaded";
   }),
   getAppSettings: f.handler.query(async (): Promise<AppSettings> => {
-    // TODO: Implement app settings
-    return {
-      locale: "en",
-      theme: "default",
-      network: "testnet",
-      mainnetWalletServer: "",
-      testnetWalletServer: "",
-    };
+    return await SettingsManager.db().getAllOrDefault();
   }),
   getExplorerUrl: f.handler.query(
     async (args?: {
       type: "transaction" | "block";
       hash: string;
     }): Promise<string | null> => {
-      // TODO: Handle network switching in getExplorerUrl
-      const network: string = "testnet";
+      const network = await SettingsManager.db().getOrDefault(
+        SettingsKey.Network,
+      );
 
       let url;
-      if (network === "mainnet") {
+      if (network === Network.MAINNET) {
         url = "https://explorer.ironfish.network/";
-      } else if (network === "testnet") {
+      } else if (network === Network.TESTNET) {
         url = "https://testnet.explorer.ironfish.network/";
       } else {
         return null;
@@ -45,10 +43,9 @@ export const appHandlers = f.facade<AppHandlers>({
       return url;
     },
   ),
-  setAppSettings: f.handler.mutation(
-    async (args: { settings: Partial<AppSettings> }) => {
-      // TODO: Implement app settings
-      return;
+  setAppSetting: f.handler.mutation(
+    async (args: { key: keyof AppSettings; value: string | undefined }) => {
+      await SettingsManager.db().set(args.key, args.value);
     },
   ),
 });
